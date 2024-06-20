0fedc13426d48b70c111ea33ebc3d89806308ac5
LeonHsieh <149147315+ShangLin1606@users.noreply.github.com>
2024-06-20 14:27:00 +0800

    Update stock_data_scraping.py

--- stock_data/stock_data_scraping.py
+++ stock_data/stock_data_scraping.py
@@ -1,18 +1,20 @@
-import yfinance as yf
-import os
 import csv
+import os
+
 import pandas as pd
 import talib as ta
+import yfinance as yf
+
 
 def download_stock_data(ticker, data_dir, start_date):
     try:
         df = yf.download(ticker, start=start_date)
         if df.empty:
-            return ticker, 0, '', ''
-        
+            return ticker, 0, "", ""
+
         # Calculate technical indicators
 
-        '''
+        """
         SMA(Simple Moving Average)
         EMA(Exponential Moving Average)
         RSI(Relative Strength Index)
@@ -27,54 +29,86 @@ def download_stock_data(ticker, data_dir, start_date):
         ROC(Rate of Change)
         ADX(Average directional movement index)
         DMI(Directional Movement Index)
-        '''
-        df['SMA'] = ta.SMA(df['Close'], timeperiod=30)
-        df['EMA'] = ta.EMA(df['Close'], timeperiod=30)
-        df['RSI'] = ta.RSI(df['Close'], timeperiod=14)
-        df['MACD'], df['MACD_Signal'], df['MACD_Hist'] = ta.MACD(df['Close'], fastperiod=12, slowperiod=26, signalperiod=9)
-        df['Upper_Band'], df['Middle_Band'], df['Lower_Band'] = ta.BBANDS(df['Close'], timeperiod=20)
-        df['Stochastic_K'], df['Stochastic_D'] = ta.STOCH(df['High'], df['Low'], df['Close'], fastk_period=14, slowk_period=3, slowd_period=3)
-        df['Williams_%R'] = ta.WILLR(df['High'], df['Low'], df['Close'], timeperiod=14)
-        df['ATR'] = ta.ATR(df['High'], df['Low'], df['Close'], timeperiod=14)
-        df['MFI'] = ta.MFI(df['High'], df['Low'], df['Close'], df['Volume'], timeperiod=14)
-        df['OBV'] = ta.OBV(df['Close'], df['Volume'])
-        df['Momentum'] = ta.MOM(df['Close'], timeperiod=10)
-        df['ROC'] = ta.ROC(df['Close'], timeperiod=10)
-        df['ADX'] = ta.ADX(df['High'], df['Low'], df['Close'], timeperiod = 14)
-        df['PLUS_DMI'] = ta.PLUS_DI(df['High'],df['Low'],df['Close'],timeperiod=14)
-        df['MINUS_DMI'] = ta.MINUS_DI(df['High'],df['Low'],df['Close'],timeperiod=14)
-        
+        """
+        df["SMA5"] = ta.SMA(df["Close"], timeperiod=5)
+        df["SMA30"] = ta.SMA(df["Close"], timeperiod=30)
+        df["SMA60"] = ta.SMA(df["Close"], timeperiod=60)
+        df["SMA180"] = ta.SMA(df["Close"], timeperiod=180)
+
+        df["EMA5"] = ta.EMA(df["Close"], timeperiod=5)
+        df["EMA30"] = ta.EMA(df["Close"], timeperiod=30)
+        df["EMA60"] = ta.EMA(df["Close"], timeperiod=60)
+        df["EMA180"] = ta.EMA(df["Close"], timeperiod=180)
+
+        df["WMA5"] = ta.WMA(df["Close"], timeperiod=5)
+        df["WMA30"] = ta.WMA(df["Close"], timeperiod=30)
+        df["WMA60"] = ta.WMA(df["Close"], timeperiod=60)
+        df["WMA180"] = ta.WMA(df["Close"], timeperiod=180)
+
+        df["RSI"] = ta.RSI(df["Close"], timeperiod=180)
+        df["MACD"], df["MACD_Signal"], df["MACD_Hist"] = ta.MACD(
+            df["Close"], fastperiod=12, slowperiod=26, signalperiod=9
+        )
+        df["Upper_Band"], df["Middle_Band"], df["Lower_Band"] = ta.BBANDS(
+            df["Close"], timeperiod=20
+        )
+        df["Stochastic_K"], df["Stochastic_D"] = ta.STOCH(
+            df["High"],
+            df["Low"],
+            df["Close"],
+            fastk_period=14,
+            slowk_period=3,
+            slowd_period=3,
+        )
+        df["Williams_R"] = ta.WILLR(df["High"], df["Low"], df["Close"], timeperiod=14)
+        df["ATR"] = ta.ATR(df["High"], df["Low"], df["Close"], timeperiod=14)
+        df["MFI"] = ta.MFI(
+            df["High"], df["Low"], df["Close"], df["Volume"], timeperiod=14
+        )
+        df["OBV"] = ta.OBV(df["Close"], df["Volume"])
+        df["Momentum"] = ta.MOM(df["Close"], timeperiod=10)
+        df["ROC"] = ta.ROC(df["Close"], timeperiod=10)
+        df["ADX"] = ta.ADX(df["High"], df["Low"], df["Close"], timeperiod=14)
+        df["PLUS_DMI"] = ta.PLUS_DI(df["High"], df["Low"], df["Close"], timeperiod=14)
+        df["MINUS_DMI"] = ta.MINUS_DI(df["High"], df["Low"], df["Close"], timeperiod=14)
+
         # Drop rows with any NaN values
         df.dropna(inplace=True)
-        
+
         if df.empty:
-            return ticker, 0, '', ''
-        
+            return ticker, 0, "", ""
+
         # Save data with indicators
-        df.to_csv(os.path.join(data_dir, ticker + '.csv'))
-        
-        first_date = df.index[0].strftime('%Y-%m-%d')
-        last_date = df.index[-1].strftime('%Y-%m-%d')
+        df.to_csv(f"{data_dir}/{ticker}.csv")
+
+        first_date = df.index[0].strftime("%Y-%m-%d")
+        last_date = df.index[-1].strftime("%Y-%m-%d")
         return ticker, len(df), first_date, last_date
     except Exception as e:
-        return ticker, f'Error: {e}', '', ''
+        return ticker, f"Error: {e}", "", ""
+
 
 # Main code to read tickers and call the function
-ticker_list_path = 'yh_all_symbols.txt'
-data_dir = 'stock_data'
-output_file = 'stock_data_lengths.csv'
-start_date = '1980-06-01'
+ticker_list_path = "stock_stock.txt"
+data_dir = "stock_data_model"
+output_file = "stock_data_lengths_model.csv"
+start_date = "1980-06-01"
 
 # Make a directory
-os.makedirs(data_dir, exist_ok=True)
+if __name__ == "__main__":
+    os.makedirs(data_dir, exist_ok=True)
 
-with open(ticker_list_path, 'r') as f, open(output_file, 'w', newline='') as csvfile:
-    lines = f.readlines()
-    writer = csv.writer(csvfile)
-    writer.writerow(['Ticker', 'Data Length', 'First Date', 'Last Date'])  # Write header
-    for line in lines:
-        ticker = line.strip("\n")
-        print(f'Processing {ticker}...')
-        result = download_stock_data(ticker, data_dir, start_date)
-        writer.writerow(result)
-        print(f'Data for {ticker} processed: {result[1]} rows')
+    with open(ticker_list_path, "r") as f, open(
+    output_file, "w", encoding="utf-8"
+) as csvfile:
+        lines = f.readlines()
+        writer = csv.writer(csvfile)
+        writer.writerow(
+        ["Ticker", "Data Length", "First Date", "Last Date"]
+    )  # Write header
+        for line in lines:
+            ticker = line.strip("\n")
+            print(f"Processing {ticker}...")
+            result = download_stock_data(ticker, data_dir, start_date)
+            writer.writerow(result)
+            print(f"Data for {ticker} processed: {result[1]} rows")
