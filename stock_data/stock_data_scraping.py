754869a6a197970990a3ef940b65dd9537be663b
LeonHsieh <149147315+ShangLin1606@users.noreply.github.com>
2024-06-28 14:59:42 +0800

    Update stock_data_scraping.py

--- stock_data/stock_data_scraping.py
+++ stock_data/stock_data_scraping.py
@@ -6,9 +6,9 @@ import talib as ta
 import yfinance as yf
 
 
-def download_stock_data(ticker, data_dir, start_date):
+def download_stock_data(ticker, data_dir, start_date, end_date):
     try:
-        df = yf.download(ticker, start=start_date)
+        df = yf.download(ticker, start=start_date, end=end_date)
         if df.empty:
             return ticker, 0, "", ""
 
@@ -90,39 +90,43 @@ def download_stock_data(ticker, data_dir, start_date):
         df["Adj Close_EMA120"] = ta.EMA(df["Adj Close"], timeperiod=120)
         df["Adj Close_EMA180"] = ta.EMA(df["Adj Close"], timeperiod=180)
 
-        df["Close_WMA5"] = ta.WMA(df["Close"], timeperiod=5)
-        df["Close_WMA20"] = ta.WMA(df["Close"], timeperiod=20)
-        df["Close_WMA60"] = ta.WMA(df["Close"], timeperiod=60)
-        df["Close_WMA120"] = ta.WMA(df["Close"], timeperiod=120)
-        df["Close_WMA180"] = ta.WMA(df["Close"], timeperiod=180)
+        # df["Close_WMA5"] = ta.WMA(df["Close"], timeperiod=5)
+        # df["Close_WMA20"] = ta.WMA(df["Close"], timeperiod=20)
+        # df["Close_WMA60"] = ta.WMA(df["Close"], timeperiod=60)
+        # df["Close_WMA120"] = ta.WMA(df["Close"], timeperiod=120)
+        # df["Close_WMA180"] = ta.WMA(df["Close"], timeperiod=180)
 
-        df["Open_WMA5"] = ta.WMA(df["Open"], timeperiod=5)
-        df["Open_WMA20"] = ta.WMA(df["Open"], timeperiod=20)
-        df["Open_WMA60"] = ta.WMA(df["Open"], timeperiod=60)
-        df["Open_WMA120"] = ta.WMA(df["Open"], timeperiod=120)
-        df["Open_WMA180"] = ta.WMA(df["Open"], timeperiod=180)
+        # df["Open_WMA5"] = ta.WMA(df["Open"], timeperiod=5)
+        # df["Open_WMA20"] = ta.WMA(df["Open"], timeperiod=20)
+        # df["Open_WMA60"] = ta.WMA(df["Open"], timeperiod=60)
+        # df["Open_WMA120"] = ta.WMA(df["Open"], timeperiod=120)
+        # df["Open_WMA180"] = ta.WMA(df["Open"], timeperiod=180)
 
-        df["High_WMA5"] = ta.WMA(df["High"], timeperiod=5)
-        df["High_WMA20"] = ta.WMA(df["High"], timeperiod=20)
-        df["High_WMA60"] = ta.WMA(df["High"], timeperiod=60)
-        df["High_WMA120"] = ta.WMA(df["High"], timeperiod=120)
-        df["High_WMA180"] = ta.WMA(df["High"], timeperiod=180)
+        # df["High_WMA5"] = ta.WMA(df["High"], timeperiod=5)
+        # df["High_WMA20"] = ta.WMA(df["High"], timeperiod=20)
+        # df["High_WMA60"] = ta.WMA(df["High"], timeperiod=60)
+        # df["High_WMA120"] = ta.WMA(df["High"], timeperiod=120)
+        # df["High_WMA180"] = ta.WMA(df["High"], timeperiod=180)
 
-        df["Low_WMA5"] = ta.WMA(df["Low"], timeperiod=5)
-        df["Low_WMA20"] = ta.WMA(df["Low"], timeperiod=20)
-        df["Low_WMA60"] = ta.WMA(df["Low"], timeperiod=60)
-        df["Low_WMA120"] = ta.WMA(df["Low"], timeperiod=120)
-        df["Low_WMA180"] = ta.WMA(df["Low"], timeperiod=180)
+        # df["Low_WMA5"] = ta.WMA(df["Low"], timeperiod=5)
+        # df["Low_WMA20"] = ta.WMA(df["Low"], timeperiod=20)
+        # df["Low_WMA60"] = ta.WMA(df["Low"], timeperiod=60)
+        # df["Low_WMA120"] = ta.WMA(df["Low"], timeperiod=120)
+        # df["Low_WMA180"] = ta.WMA(df["Low"], timeperiod=180)
 
-        df["Adj Close_WMA5"] = ta.WMA(df["Adj Close"], timeperiod=5)
-        df["Adj Close_WMA20"] = ta.WMA(df["Adj Close"], timeperiod=20)
-        df["Adj Close_WMA60"] = ta.WMA(df["Adj Close"], timeperiod=60)
-        df["Adj Close_WMA120"] = ta.WMA(df["Adj Close"], timeperiod=120)
-        df["Adj Close_WMA180"] = ta.WMA(df["Adj Close"], timeperiod=180)
+        # df["Adj Close_WMA5"] = ta.WMA(df["Adj Close"], timeperiod=5)
+        # df["Adj Close_WMA20"] = ta.WMA(df["Adj Close"], timeperiod=20)
+        # df["Adj Close_WMA60"] = ta.WMA(df["Adj Close"], timeperiod=60)
+        # df["Adj Close_WMA120"] = ta.WMA(df["Adj Close"], timeperiod=120)
+        # df["Adj Close_WMA180"] = ta.WMA(df["Adj Close"], timeperiod=180)
 
 
 
-        df["RSI"] = ta.RSI(df["Close"], timeperiod=180)
+        df["RSI6"] = ta.RSI(df["Close"], timeperiod=6)#短期
+        df["RSI14"] = ta.RSI(df["Close"], timeperiod=14)#一般使用14
+        df["RSI24"] = ta.RSI(df["Close"], timeperiod=24)#長期
+
+
         df["MACD"], df["DIF"], df["OSC_Hist"] = ta.MACD(
             df["Close"], fastperiod=12, slowperiod=26, signalperiod=9
         )
@@ -148,10 +152,14 @@ def download_stock_data(ticker, data_dir, start_date):
         df["ADX"] = ta.ADX(df["High"], df["Low"], df["Close"], timeperiod=14)
         df["PLUS_DMI"] = ta.PLUS_DI(df["High"], df["Low"], df["Close"], timeperiod=14)
         df["MINUS_DMI"] = ta.MINUS_DI(df["High"], df["Low"], df["Close"], timeperiod=14)
+        df["Daily_Return"] = df["Close"].pct_change()
+        df["Daily_Change"] = df["Close"].diff()
+        df["STD"] = df["Close"].std()
+
         # df["symbol"] = ticker
 
         # Drop rows with any NaN values
-        df.dropna(inplace=True)
+        # df.dropna(inplace=True)
 
         if df.empty:
             return ticker, 0, "", ""
@@ -171,6 +179,7 @@ ticker_list_path = "stock_tickers.txt"
 data_dir = "stock_data_model"
 output_file = "stock_data_lengths_model.csv"
 start_date = "1993-11-06"
+end_date = "2024-06-21"
 
 # Make a directory
 if __name__ == "__main__":
@@ -187,6 +196,7 @@ if __name__ == "__main__":
         for line in lines:
             ticker = line.strip("\n")
             print(f"Processing {ticker}...")
-            result = download_stock_data(ticker, data_dir, start_date)
+            result = download_stock_data(ticker, data_dir, start_date, end_date)
             writer.writerow(result)
             print(f"Data for {ticker} processed: {result[1]} rows")
+
