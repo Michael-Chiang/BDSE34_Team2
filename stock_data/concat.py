31aeb4dfdc2f710fccf097f7a5b6e268867da32b
Kevin <s986103kevin@gmail.com>
2024-07-01 12:16:24 +0800

    delete unused files

--- stock_data/concat.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-from glob import glob
- 
-files = glob('stock*.csv')
- 
-df = pd.concat((pd.read_csv(file, dtype={'Ticker': str}) for file in files), axis='columns')
-df.to_csv('stock_info_all.csv')
