5a0add87acc2970fd47b6465ead92f69f688b880
Kevin <s986103kevin@gmail.com>
2024-07-01 20:59:42 +0800

    update K-means for Finance & Technology 1. adpative yr setting 2. plot figure of close price

--- src/KMeans_clustering.py
+++ src/KMeans_clustering.py
@@ -4,6 +4,9 @@ import os
 import glob
 import joblib
 import logging
+import matplotlib.pyplot as plt
+import matplotlib.dates as mdates
+from datetime import datetime, timedelta
 
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
@@ -19,43 +22,73 @@ def read_and_filter_data(csv_file_path, start_date, end_date):
     data = pd.read_csv(csv_file_path)
     filtered_data = data[(data['Date'] >= start_date) & (
         data['Date'] <= end_date)][['Open', 'High', 'Low', 'Close']]
-    return filtered_data
+    date_range = data[(data['Date'] >= start_date) & (
+        data['Date'] <= end_date)][['Date']]
+    return filtered_data, date_range
 
 
-def standardize_data(data):
+def standardize_data(data, stockID, date_range, stock_fig_dir):
     """Standardize the data."""
     scaler = StandardScaler()
     standardized_data = scaler.fit_transform(data)
+
+    # Plot the 'Close' column
+    plt.figure(figsize=(10, 6))
+    # Column 3 corresponds to 'Close'
+    # print(f"date_range = {date_range}, close = {standardized_data[:, 3]}")
+    # print(date_range.to_numpy().shape)
+    # print(standardized_data[:, 3].shape)
+    plt.plot(date_range.to_numpy().reshape(-1),
+             standardized_data[:, 3].tolist())
+    plt.xlabel('Date')
+    plt.ylabel('Standardized Close Value')
+    plt.title(f'Standardized Close Prices for {stockID}')
+    # Set date format on x-axis to make it less dense
+    plt.gca().xaxis.set_major_locator(
+        mdates.MonthLocator(interval=3))  # Show every second week
+    plt.xticks(rotation=45)
+    plt.tight_layout()
+    plt.savefig(os.path.join(stock_fig_dir, f'{stockID}.jpg'))
+    plt.close()
     return standardized_data
 
 
-def prepare_data_for_clustering(sectors, start_date, end_date):
+def prepare_data_for_clustering(sector, start_date, end_date, stock_fig_dir):
     """Prepare the data for clustering."""
     logging.info('Preparing data for clustering')
     X = []
-    golden_shape = get_golden_length(start_date, end_date)
-    for sector in sectors:
-        csv_file_paths = glob.glob(os.path.join(
-            f'stock_data_model/{sector}', '*.csv'))
-        for csv_file_path in csv_file_paths:
-            stockID = os.path.basename(csv_file_path).split('.')[0]
-            filtered_data = read_and_filter_data(
-                csv_file_path, start_date, end_date)
-            if filtered_data.shape != golden_shape:
-                print(filtered_data.shape)
-                continue
-            standardized_data = standardize_data(filtered_data)
+    other_cluster = []
 
-            data_entry = [stockID] + standardized_data.reshape(-1).tolist()
-            X.append(data_entry)
+    golden_shape = get_golden_length(start_date, end_date, sector)
+
+    csv_file_paths = glob.glob(os.path.join(
+        f'stock_data_model/{sector}', '*.csv'))
+    for csv_file_path in csv_file_paths:
+        stockID = os.path.basename(csv_file_path).split('.')[0]
+        filtered_data, date_range = read_and_filter_data(
+            csv_file_path, start_date, end_date)
+        if filtered_data.shape != golden_shape:
+            print(filtered_data.shape)
+            other_cluster.append(stockID)
+            continue
+        standardized_data = standardize_data(
+            filtered_data, stockID, date_range, stock_fig_dir)
+
+        data_entry = [stockID] + standardized_data.reshape(-1).tolist()
+        X.append(data_entry)
     logging.info(f'Prepared data for {len(X)} stocks')
-    return np.array(X, dtype=object)
+    return np.array(X, dtype=object), other_cluster
 
 
-def get_golden_length(start_date, end_date):
-    csv_file_path = os.path.join(
-        f'stock_data_model/', 'Finance', 'AAME.csv')
-    filtered_data = read_and_filter_data(csv_file_path, start_date, end_date)
+def get_golden_length(start_date, end_date, sector):
+    if sector == 'Finance':
+        csv_file_path = os.path.join(
+            f'stock_data_model/', sector, 'AAME.csv')
+    else:
+        csv_file_path = os.path.join(
+            f'stock_data_model/', sector, 'AAPL.csv')
+    filtered_data, _ = read_and_filter_data(
+        csv_file_path, start_date, end_date)
     return filtered_data.shape
 
 
@@ -81,32 +114,64 @@ def load_model(model_path):
     return joblib.load(model_path)
 
 
-def save_clustering_results(X, labels, output_file):
+def save_clustering_results(X, labels, output_file, other_cluster):
     """Save the clustering results to a CSV file."""
     logging.info(f'Saving clustering results to {output_file}')
     df = pd.DataFrame()
     df['stockID'] = X[:, 0]
     df['clusterID'] = labels
+    # Adding other_cluster
+    other_cluster_df = pd.DataFrame(other_cluster, columns=['stockID'])
+    # Assign a clusterID that indicates it's in other_cluster
+    other_cluster_df['clusterID'] = -1
+
+    # Concatenate the original dataframe with the other_cluster dataframe
+    df = pd.concat([df, other_cluster_df], ignore_index=True)
+
     df.to_csv(output_file, index=False)
+
+
+def calculate_start_date(end_date, years_before):
+    """Calculate the start date based on the end date and the number of years before."""
+    end_date_obj = datetime.strptime(end_date, '%Y-%m-%d')
+    start_date_obj = end_date_obj - timedelta(days=365*years_before)
+    return start_date_obj.strftime('%Y-%m-%d')
 
 
 def main():
     logging.info('Starting clustering process')
-    sectors = ['Finance']
-    start_date = '2023-06-20'
+    sectors = ['Finance', 'Technology']
     end_date = '2024-06-20'
+    years_before = 4  # This can be adjusted based on user input
+    start_date = calculate_start_date(end_date, years_before)
+    logging.info(f'start_date = {start_date}')
+
     K = 5
-    model_path = 'model/kmeans_model.pkl'
-    output_file = 'clustering_result.csv'
+    model_dir = 'model/'
+    stock_fig_dir = 'stock_figure/'
+    output_dir = 'clustering_results/'
 
-    X = prepare_data_for_clustering(sectors, start_date, end_date)
-    kmeans, labels = perform_clustering(X, K)
-    save_clustering_results(X, labels, output_file)
-    save_model(kmeans, model_path)
+    if not os.path.exists(model_dir):
+        os.makedirs(model_dir)
+    if not os.path.exists(stock_fig_dir):
+        os.makedirs(stock_fig_dir)
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
 
-    # Load the model (for testing purposes)
-    loaded_kmeans = load_model(model_path)
-    logging.info('Model loaded successfully')
+    for sector in sectors:
+        model_path = os.path.join(model_dir, f'kmeans_model_{sector}.pkl')
+        output_file = os.path.join(
+            output_dir, f'clustering_result_{sector}.csv')
+
+        X, other_cluster = prepare_data_for_clustering(
+            sector, start_date, end_date, stock_fig_dir)
+        kmeans, labels = perform_clustering(X, K)
+        save_clustering_results(X, labels, output_file, other_cluster)
+        save_model(kmeans, model_path)
+
+        # Load the model (for testing purposes)
+        loaded_kmeans = load_model(model_path)
+        logging.info('Model loaded successfully')
 
 
 if __name__ == "__main__":
