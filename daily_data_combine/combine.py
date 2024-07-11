b427444fcb3ff8cc04769cabf4835a07c5cf1d09
Michael-Chiang <56470587+Michael-Chiang@users.noreply.github.com>
2024-07-11 16:32:54 +0800

    Add files via upload

--- daily_data_combine/combine.py
+++ daily_data_combine/combine.py
@@ -14,14 +14,17 @@ def merge_files(file_a, file_b, output_file):
     if not df_b.index.is_unique:
         df_b = df_b.set_index(['Date', df_b.columns[1]], drop=False)
 
-    # Drop extra dates from file A
-    extra_dates = df_a.index[~df_a.index.isin(df_b.index)]
-    df_a.drop(extra_dates, inplace=True)
+    # # Drop extra dates from file A
+    # extra_dates = df_a.index[~df_a.index.isin(df_b.index)]
+    # df_a.drop(extra_dates, inplace=True)
 
-    # Merge DataFrames
-    merged_df = df_b.join(df_a, how='left', on='Date')
+    # # Merge DataFrames
+    # merged_df = df_b.join(df_a, how='left', on='Date')
+
+    merged_df = df_b.merge(df_a, on="Date")
 
     merged_df = merged_df.fillna(method='bfill')
+
 
     # Save merged data
     merged_df.to_csv(output_file)
