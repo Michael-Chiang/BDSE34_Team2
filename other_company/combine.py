33af860ff7c90dfd82f88ab78b3c158a59117212
Michael-Chiang <56470587+Michael-Chiang@users.noreply.github.com>
2024-07-13 05:16:53 +0800

    Add files via upload

--- other_company/combine.py
+++ other_company/combine.py
@@ -1,54 +1,12 @@
-# import os
-
-# import numpy as np
-# import pandas as pd
-
-# with open("other_company.txt", "r") as f:
-#     company = [line.strip() for line in f]
-
-
-# def merge_files(file_a, file_b, output_file):
-#     # Read files into DataFrames
-#     df_a = pd.read_csv(file_a, index_col='Date', parse_dates=True)
-#     df_a.drop(columns=['Open', 'High','Low', 'Adj Close', 'Volume'], inplace=True)
-#     df_a.rename(columns={'Close': company}, inplace=True)
-
-#     df_b = pd.read_csv(file_b, index_col='Date', parse_dates=True)
-
-#     # Handle non-unique index in file B (optional)
-#     if not df_b.index.is_unique:
-#         df_b = df_b.set_index(['Date', df_b.columns[1]], drop=False)
-
-#     # Drop extra dates from file A
-#     extra_dates = df_a.index[~df_a.index.isin(df_b.index)]
-#     df_a.drop(extra_dates, inplace=True)
-    
-#     print(df_a)
-#     print(df_a.dtypes)
-
-
-#     # Merge DataFrames
-#     merged_df = df_b.join(df_a, how='left', on='Date')
-
-#     merged_df = merged_df.fillna(method='bfill')
-
-#     # Save merged data
-#     merged_df.to_csv(output_file)
-
-
-# # Replace 'file_a.csv' and 'file_b.csv' with your actual file paths
-# file_a = f'{company}.csv'
-# file_b = 'other_company.csv'
-# output_file = 'other_company.csv'
-
-# merge_files(file_a, file_b, output_file)
-
-
-
 import os
 
 import numpy as np
 import pandas as pd
+
+aa = pd.read_csv('other_company.csv')
+aa = aa[['Date']]
+aa = aa.set_index('Date')
+aa.to_csv('other_company.csv')
 
 
 def merge_files(file_a, file_b, output_file):
@@ -71,10 +29,6 @@ def merge_files(file_a, file_b, output_file):
     # Handle non-unique index in file B (optional)
     if not df_b.index.is_unique:
         df_b = df_b.set_index(['Date', df_b.columns[1]], drop=False)
-
-    # Drop extra dates from file A
-    extra_dates = df_a.index[~df_a.index.isin(df_b.index)]
-    df_a.drop(extra_dates, inplace=True)
 
     # Merge DataFrames
     merged_df = df_b.join(df_a, how='left', on='Date')
@@ -102,5 +56,3 @@ for company in companies:
     print(f"Merged data for {company}")
 
 print("Process completed!")
-
-
