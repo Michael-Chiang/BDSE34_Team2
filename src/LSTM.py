25f07c1cf6f9652e6e4bf3d5936ada5471b9ca46
Kevin <s986103kevin@gmail.com>
2024-07-12 13:06:41 +0800

    add pydantic argparse for parameters modify 1D CNN input order

--- src/LSTM.py
+++ src/LSTM.py
@@ -15,32 +15,60 @@ from sklearn.metrics import classification_report, confusion_matrix
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler, StandardScaler
 from torch.utils.data import DataLoader, TensorDataset
+from torchsummary import summary
+
+from pydantic.v1 import BaseModel, Field, conint, root_validator
+from typing import Literal
+import pydantic_argparse
 
 from my_utils.models import LSTM
 
-# Get current time for log file name
-current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
-log_dir = "log"
-os.makedirs(log_dir, exist_ok=True)
-log_filename = os.path.join(log_dir, f"log_{current_time}.txt")
 
-# Set up logging
-logging.basicConfig(
-    level=logging.INFO,
-    format="%(asctime)s %(message)s",
-    handlers=[
-        logging.StreamHandler(),  # Log to console
-        logging.FileHandler(log_filename),  # Log to file]
-    ],
-)
+class Config(BaseModel):
+    input_dim: int = Field(71, description="Number of features")
+    hidden_dim: int = Field(100, description="Hidden layer dimension")
+    num_layers: conint(ge=1, le=5) = Field(  # type: ignore
+        2, description="Number of LSTM layers")
+    output_dim: int = Field(5, description="Output dimension")
+    batch_size: int = Field(64, description="Batch size")
+    num_epochs: int = Field(200, description="Number of epochs")
+    lr: float = Field(0.0001, description="Learning rate")
+    patience: int = Field(20, description="Early stopping patience")
+    step_size: int = Field(20, description="Learning rate scheduler step size")
+    gamma: float = Field(
+        0.8, description="Learning rate scheduler weight decay")
+
+    sector: Literal['Finance', 'Technology'] = Field(
+        description="The name must be 'Finance' or 'Technology'")
+    lookback: int = Field(None, description="Sequence length")
+    interval: int = Field(None, description="Sample days difference")
+    period: int = Field(None, description="Predicted days after")
+
+    @root_validator(pre=True)
+    def set_defaults(cls, values):
+        name = values.get('sector')
+        if name == 'Finance':
+            values['lookback'] = 60
+            values['interval'] = 10
+            values['period'] = 10
+        elif name == 'Technology':
+            values['lookback'] = 20
+            values['interval'] = 5
+            values['period'] = 5
+        return values
+    clusterID: conint(ge=0, le=4) = Field(  # type: ignore
+        description="Cluster ID")  # type: ignore
+
 
 # Set device
 use_cuda = 1
-device = torch.device("cuda" if (torch.cuda.is_available() & use_cuda) else "cpu")
+device = torch.device("cuda" if (
+    torch.cuda.is_available() & use_cuda) else "cpu")
 logging.info(f"Device: {device}")
 
-
 # Data transformation functions
+
+
 def transform_type(x, device, is_train=True):
     tensor = torch.Tensor(x.astype(float)).to(device)
     return tensor if is_train else tensor.to(torch.int64)
@@ -51,7 +79,7 @@ def split_data(stock, lookback, interval, y):
     n_time = len(data_raw)
     data, targets = [], []
     for index in range(0, n_time - lookback, interval):
-        data.append(data_raw[index : index + lookback, :])
+        data.append(data_raw[index: index + lookback, :])
         targets.append(y.iloc[index + lookback])
 
     data = np.array(data)
@@ -184,7 +212,8 @@ def show_accuracy(model, dl):
         for batch_x, batch_y in dl:
             pred_y = model(batch_x)
             correct += (
-                (torch.argmax(pred_y, dim=1) == batch_y).type(torch.float).sum().item()
+                (torch.argmax(pred_y, dim=1) == batch_y).type(
+                    torch.float).sum().item()
             )
     return correct / size
 
@@ -206,7 +235,8 @@ def save_confusion_matrix(model, train_dl, test_dl, path):
                     .item()
                 )
                 all_batch_y.append(batch_y.cpu().numpy())
-                all_batch_y_pred.append(torch.argmax(pred_y, dim=1).cpu().numpy())
+                all_batch_y_pred.append(
+                    torch.argmax(pred_y, dim=1).cpu().numpy())
 
             if dl == train_dl:
                 y_train = np.concatenate(all_batch_y)
@@ -301,54 +331,50 @@ def prepare_data(file_paths, lookback, interval, period):
     return x_train_, y_train_, x_test_, y_test_
 
 
-def main():
-    # Parameters
-    input_dim = 71  # Number of features
-    hidden_dim = 100
-    num_layers = 2
-    output_dim = 5
-    batch_size = 64
-    num_epochs = 200
-    lr = 0.0001
-    patience = 20
-    lookback = 20  # Sequence length
-    interval = 5  # sample days difference
-    period = 5  # predicted days after
-    sector = "Technology"
-    clusterID = "4"
-    file_paths = glob.glob(os.path.join("stock_data_all", sector, clusterID, "*.csv"))
-    model_save_path = f"model/best_lstm_model_{sector}_{clusterID}.pth"
-    figure_save_path = f"figure/confusion_matrix_{sector}_{clusterID}.jpg"
+def main(config: Config) -> None:
+
+    clusterID = str(config.clusterID)
+    file_paths = glob.glob(os.path.join(
+        "stock_data_all", config.sector, clusterID, "*.csv"))
+    model_path = "model/"
+    figure_path = "figure/"
+    model_save_path = f"model/best_lstm_model_{config.sector}_{clusterID}.pth"
+    figure_save_path = f"figure/confusion_matrix_best_lstm_{config.sector}_{clusterID}.jpg"
+
+    os.makedirs(model_path, exist_ok=True)
+    os.makedirs(figure_path, exist_ok=True)
 
     # Prepare data
     x_train_, y_train_, x_test_, y_test_ = prepare_data(
-        file_paths, lookback, interval, period
+        file_paths, config.lookback, config.interval, config.period
     )
 
     # Data loaders
     train_ds = TensorDataset(x_train_, y_train_)
     test_ds = TensorDataset(x_test_, y_test_)
-    train_dl = DataLoader(train_ds, batch_size=batch_size, shuffle=True)
-    test_dl = DataLoader(test_ds, batch_size=batch_size, shuffle=False)
+    train_dl = DataLoader(train_ds, batch_size=config.batch_size, shuffle=True)
+    test_dl = DataLoader(test_ds, batch_size=config.batch_size, shuffle=False)
 
     # Initialize model
     model = LSTM(
-        input_dim=input_dim,
-        hidden_dim=hidden_dim,
-        output_dim=output_dim,
-        num_layers=num_layers,
-        seq_length=lookback,
+        input_dim=config.input_dim,
+        hidden_dim=config.hidden_dim,
+        output_dim=config.output_dim,
+        num_layers=config.num_layers,
+        seq_length=config.lookback,
     ).to(device)
+    logging.info(summary(model, (config.lookback, config.input_dim)))
     criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.AdamW(model.parameters(), lr=lr)
-    scheduler = torch.optim.lr_scheduler.StepLR(optimizer, step_size=20, gamma=0.8)
+    optimizer = torch.optim.AdamW(model.parameters(), lr=config.lr)
+    scheduler = torch.optim.lr_scheduler.StepLR(
+        optimizer, step_size=config.step_size, gamma=config.gamma)
 
     # Train model
     start_time = time.time()
     best_model, hist = train(
         model,
-        num_epochs,
-        patience,
+        config.num_epochs,
+        config.patience,
         train_dl,
         test_dl,
         device,
@@ -358,14 +384,40 @@ def main():
     )
     training_time = time.time() - start_time
     logging.info(f"Training time: {training_time}")
-    logging.info(f"Training accuracy: {show_accuracy(best_model, train_dl):.4f}")
+    logging.info(
+        f"Training accuracy: {show_accuracy(best_model, train_dl):.4f}")
     logging.info(f"Testing accuracy: {show_accuracy(best_model, test_dl):.4f}")
 
     # Save model
     save_model(best_model, model_save_path)
-    load_model(model_save_path, input_dim, hidden_dim, num_layers, output_dim, lookback)
+    load_model(model_save_path, config.input_dim, config.hidden_dim,
+               config.num_layers, config.output_dim, config.lookback)
     save_confusion_matrix(model, train_dl, test_dl, figure_save_path)
 
 
 if __name__ == "__main__":
-    main()
+    parser = pydantic_argparse.ArgumentParser(
+        model=Config,
+        prog="LSTM Training Program",
+        description="Train LSTM model with given configuration",
+    )
+    config = parser.parse_typed_args()
+
+    # Get current time for log file name
+    current_time = datetime.now().strftime("%Y%m%d_%H%M%S")
+    log_dir = "log"
+    os.makedirs(log_dir, exist_ok=True)
+    log_filename = os.path.join(
+        log_dir, f"log_{current_time}_{config.sector}_{config.clusterID}.txt")
+
+    # Set up logging
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s %(message)s",
+        force=True,
+        handlers=[
+            logging.StreamHandler(),  # Log to console
+            logging.FileHandler(log_filename),  # Log to file]
+        ],
+    )
+    main(config)
--- src/my_utils/models.py
+++ src/my_utils/models.py
@@ -4,7 +4,8 @@ import torch.nn as nn
 
 # Set device
 use_cuda = 1
-device = torch.device("cuda" if (torch.cuda.is_available() & use_cuda) else "cpu")
+device = torch.device("cuda" if (
+    torch.cuda.is_available() & use_cuda) else "cpu")
 
 
 class LSTM(nn.Module):
@@ -34,8 +35,10 @@ class LSTM(nn.Module):
         )
 
     def forward(self, x):
-        h0 = torch.zeros(self.num_layers * 2, x.size(0), self.hidden_dim).to(device)
-        c0 = torch.zeros(self.num_layers * 2, x.size(0), self.hidden_dim).to(device)
+        h0 = torch.zeros(self.num_layers * 2, x.size(0),
+                         self.hidden_dim).to(device)
+        c0 = torch.zeros(self.num_layers * 2, x.size(0),
+                         self.hidden_dim).to(device)
         out, _ = self.lstm(x, (h0, c0))
         # out = out.contiguous().view(x.size(0), -1)
         out = self.fc(out[:, -1, :])
@@ -49,14 +52,14 @@ class ConvBlock(nn.Module):
         out_channels,
         kernel_size=3,
         pool_kernel_size=2,
-        pool_stride=2,
         dropout_rate=0.5,
     ):
         super(ConvBlock, self).__init__()
         self.conv = nn.Conv1d(
-            in_channels, out_channels, kernel_size=kernel_size, padding=kernel_size // 2
+            in_channels, out_channels, kernel_size=kernel_size, padding=1
         )
-        self.pool = nn.MaxPool1d(kernel_size=pool_kernel_size, stride=pool_stride)
+        self.pool = nn.MaxPool1d(
+            kernel_size=pool_kernel_size)
         self.bn = nn.BatchNorm1d(in_channels)
         self.dropout = nn.Dropout1d(p=dropout_rate)
         self.relu = nn.ReLU()
@@ -88,9 +91,6 @@ class CNN(nn.Module):
         )  # 全连接层
 
     def forward(self, x):
-        x = x.transpose(
-            1, 2
-        )  # 调整形状以适应Conv1d层 (batch_size, num_features, sequence_length)
         x = self.block1(x)
         x = self.block2(x)
         x = self.block3(x)
